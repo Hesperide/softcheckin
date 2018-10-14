8423a66df60f5c64af706a288d64d46fe17e9ac9
hesperide <alex.cuoci@hotmail.ca>
2018-10-13 22:28:41 -0400

    Created client React project, separated client from server.

---
+++ server/graphql/resolvers.js
@@ -0,0 +1,72 @@
+const { GraphQLScalarType } = require('graphql');
+const { Kind } = require('graphql/language');
+const resolvers = (models) => ({
+	Query: {
+		users(){
+			return models.User.find().then((response) => response);
+		},
+		peers(){
+			return models.Peer.find().then((response) => response);
+		},
+		entries(){
+			return models.Entry.find().then((response) => response);
+		},
+		getPeerById(parent, { id }){
+			return models.Peer.findById(id).then((response) => response);
+		},
+
+		getPeerByName(parent, { userId, name }){
+			return models.Peer.findOne({ userId: userId, name: name}).then((response) => response);
+		},
+		getMyPeers(parent, { userId }){
+			return models.Peer.find({ userId: userId }).then((response) => response);
+		},
+		getPeerEntries(parent, { peerId }){
+			return models.Entry.find({ peerId: peerId }).then((response) => response);
+		},
+	},
+	Mutation: {
+		createUser(parent, args){
+			const user = new models.User(args);
+			return user.save().then((response) => response);
+		},
+		createPeer(parent, args){
+			const peer = new models.Peer(args);
+			return peer.save().then((response) => response);
+		},
+		createEntry(parent, args){
+			const entry = new models.Entry(args);
+			entry.created = new Date();
+			const peer = models.Peer.findByIdAndUpdate(args.peerId, { $set: { lastCheckIn: entry.created } }).then(response => response);
+			console.log(peer);
+			return entry.save().then((response) => response);
+		},
+		deleteUser(parent, args){
+			return models.User.deleteOne( { _id: args.userId }).then( () => true);
+		},
+		deletePeer(parent, args){
+			return models.Peer.deleteOne( { _id: args.peerId }).then( () => true);
+		},
+		deleteEntry(parent, args){
+			return models.Entry.deleteOne( { _id: args.entryId }).then( () => true);
+		}
+	},
+	Date: new GraphQLScalarType({
+		name: 'Date',
+		description: 'Date custom scalar type',
+		parseValue(val){
+			return new Date(val);
+		},
+		serialize(val){
+			return val.getTime();
+		},
+		parseLiteral(ast){
+			if (ast.kind === Kind.INT) {
+				return new Date(ast.value);
+			}
+			return null;
+		},
+	}),
+});
+
+module.exports = resolvers;
---
+++ server/index.js
@@ -0,0 +1,74 @@
+const hapi = require('hapi');
+const mongoose = require('mongoose');
+const Peer = require('./models/Peer');
+const User = require('./models/User');
+const Entry = require('./models/Entry');
+const { makeExecutableSchema } = require('graphql-tools');
+const { graphqlHapi, graphiqlHapi } = require('apollo-server-hapi');
+
+const schema = require('./graphql/schema');
+const resolvers = require('./graphql/resolvers');
+
+// ES6 Promises
+mongoose.Promise = global.Promise;
+
+// Mongo database currently local. Port to cloud later, use Azure Cosmos DB.
+mongoose.connect('mongodb://localhost/softcheckin', { useNewUrlParser: true });
+mongoose.connection.once('open', () => { console.log('connected to database'); }).on('error', (err) => { console.log(err); });
+
+const server = hapi.server({
+	port: 3001,
+	host: 'localhost'
+});
+
+const executableSchema = makeExecutableSchema({
+	typeDefs: [schema],
+	resolvers: resolvers( { Peer, User, Entry } ),
+});
+
+const init = async () => {
+
+	//Using Apollo Server 1.3.4 for GraphiQL support.
+	//To use Apollo 2.0+, need to migrate to GraphQL Playground.
+	await server.register({
+		plugin: graphiqlHapi,
+		options: {
+			path: '/graphiql',
+			graphiqlOptions: {
+				endpointURL: '/graphql'
+			},
+			route: {
+				cors: true
+			}
+		}
+	});
+	await server.register({
+		plugin: graphqlHapi,
+		options: {
+			path: '/graphql',
+			graphqlOptions: {
+				schema: executableSchema
+			},
+			route: {
+				cors: true
+			}
+		}
+
+	});
+	//Keep original route, use Hapi to load HTML files? How can I integrate this to the frontend?
+	//Don't need typical API endpoints since using GraphQL.
+	server.route([
+		{
+			method: 'GET',
+			path: '/',
+			handler: (request, reply) => {
+				return `<h1>My modern api</h1>`;
+			}
+		},
+	]);
+	
+	await server.start();
+	console.log(`Server running at: ${server.info.uri}`);
+};
+
+init();
