aadf5c12e9a4625b0e8d74f6a59171d8ec4d84d6
hesperide <alex.cuoci@hotmail.ca>
2018-10-14 00:11:47 -0400

    reading user input part-fix

--- client/src/App.js
+++ client/src/App.js
@@ -6,46 +6,50 @@ class App extends Component {
 	constructor(){
 		super();
 		this.state = {
-			name: "",
-			email: "",
+			userName: "",
+			userEmail: "",
 		};
+		this.updateInputValue = this.updateInputValue.bind(this);
+		this.createUser = this.createUser.bind(this);
 	}
 
 	updateInputValue = v => {
-		const { comp, val } = v.target;
-		console.log( v );
+		const { name, value } = v.target;
+		console.log( name );
+		console.log( v.target );
 		this.setState({
-			[comp] : val
+			[name] : value
 		});
 	};
 
 	createUser = () => {
 		const { name, email } = this.state;
 		console.log ( "Name: " + name + "." );
-		alert("New user " + name + " has been created with e-mail" + email  + ".");
+		alert("New user " + name + " has been created with e-mail " + email  + ".");
 	};
 
 	render() {
 		return (
 			<div className="App">
 				<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-		Create a new user, then check out localhost:3001/graphiql to see it!
-				</p>
-				<input
-				placeholder="Alex Cuoci"
-				value={this.state.inputValue}
-				name="name"
-				onBlur={ v => this.updateInputValue(v)}
-				/>
-				<input
-				placeholder="alex.cuoci@hotmail.ca"
-				value={this.state.inputValue}
-				name="email"
-				onBlur={ v => this.updateInputValue(v)}
-				/>
-				<button onClick={this.createUser}>Create User</button>
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Create a new user, then check out localhost:3001/graphiql to see it!
+					</p>
+					<input
+						type="text"
+						placeholder="Alex Cuoci"
+						value={this.state.value}
+						name="userName"
+						onChange={this.updateInputValue}
+					/>
+					<input
+						placeholder="alex.cuoci@hotmail.ca"
+						value={this.state.value}
+						name="userEmail"
+						onChange={ v => this.updateInputValue(v)}
+					/>
+					<button onClick={this.createUser}>Create User</button>
 				</header>
 			</div>
 		);
